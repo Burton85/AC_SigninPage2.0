9f4a2d8700b8a7563098ea13a6187d3a0bc96312
Burton <burton00tw@gmail.com>
2019-11-09 22:03:44 +0800

    Create Files

--- accountCheck.js
+++ accountCheck.js
@@ -1,48 +1,16 @@
-function checkAccount(input) {
-  const users = [
-    {
-      firstName: "Tony",
-      email: "tony@stark.com",
-      password: "iamironman"
-    },
-    {
-      firstName: "Steve",
-      email: "captain@hotmail.com",
-      password: "icandothisallday"
-    },
-    {
-      firstName: "Peter",
-      email: "peter@parker.com",
-      password: "enajyram"
-    },
-    {
-      firstName: "Natasha",
-      email: "natasha@gamil.com",
-      password: "*parol#@$!"
-    },
-    {
-      firstName: "Nick",
-      email: "nick@shield.com",
-      password: "password"
+function checkAccount(email, password) {
+  const users = require("./accounts.json").users;
+  let userId;
+  if (email && password) {
+    const user = users.find(
+      item => item.email === email && item.password === password
+    );
+    if (user) {
+      userId = user.id;
     }
-  ];
-  let result = {};
-  users.forEach(user => {
-    if (input.email === user.email) {
-      if (input.password === user.password) {
-        result.sentence = "Welcome," + user.firstName + "!";
-        result.back = false;
-      } else {
-        result.sentence = "抱歉，您輸入的密碼錯誤。";
-        result.back = true;
-      }
-    } else {
-      result.sentence = "抱歉，您輸入的帳號不存在。";
-      result.back = true;
-    }
-  });
+  }
 
-  return result;
+  return userId;
 }
 
 module.exports = checkAccount;
--- app.js
+++ app.js
@@ -1,25 +1,125 @@
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
-const port = 3000;
 const bodyParser = require("body-parser");
+//import session
+const session = require("express-session");
 const accountCheck = require("./accountCheck");
+//import accounts info
+const users = require("./accounts.json").users;
+//set the enviroment
+const {
+  port = 3000,
+  sessName = "uid",
+  sessSecret = "ssh",
+  sessLifeTime = 1000 * 60 * 60 * 2
+} = process.env;
+
+//set session
+app.use(
+  session({
+    name: sessName,
+    resave: false,
+    saveUninitialized: false,
+    secret: sessSecret,
+    cookie: {
+      maxAge: sessLifeTime,
+      sameSite: true,
+      secure: false
+    }
+  })
+);
+//Redirection
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+const redirectHome = (req, res, next) => {
+  if (req.session.userId) {
+    res.redirect("/");
+  } else {
+    next();
+  }
+};
 
 app.use(bodyParser.urlencoded({ extends: true }));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.post("/success", (req, res) => {
-  const input = req.body;
-  const result = accountCheck(input);
-  res.render("successPage", { result: result });
+//home page
+app.get("/", redirectLogin, (req, res) => {
+  const userId = req.session.userId;
+  const userInfo = users.find(item => item.id === userId);
+  res.render("home", { userInfo });
 });
 
-app.get("/", (req, res) => {
+//login page
+app.get("/login", redirectHome, (req, res) => {
   res.render("index");
+});
+//sign in
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const userId = accountCheck(email, password);
+  let errorString =
+    "Sorry!Your account is not exist or you enter the wrong password.";
+  //sign in success
+  if (userId != undefined) {
+    req.session.userId = userId;
+    return res.redirect("/");
+  }
+  //sign in fail
+  else res.render("index", { errorString });
+});
+
+//register page
+app.get("/register", redirectHome, (req, res) => {
+  res.render("register");
+});
+//register
+app.post("/register", redirectHome, (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (name && email && password) {
+    let exists = users.some(item => item.email === email);
+
+    //if email is available
+    if (!exists) {
+      let newUser = {
+        id: users.length + 1,
+        firstName: name,
+        email,
+        password
+      };
+      users.push(newUser);
+      req.session.userId = newUser.id;
+      return res.redirect("/");
+    } else {
+      let errString1 = "Sorry!This email has been already used ";
+      res.render("register", { errString1 });
+    }
+  } else {
+    let errString = "Please make sure you have fill in the form completely";
+    res.render("register", { errString });
+  }
+});
+
+//logout
+app.post("/logout", redirectLogin, (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.redirect("/");
+    }
+    res.clearCookie(sessName);
+    res.redirect("/login");
+  });
 });
 
 app.listen(port, (req, res) => {
-  console.log("this express is listening on http://localhost:3000");
+  console.log(`This express is listening on http://localhost:${port}`);
 });
